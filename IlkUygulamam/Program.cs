000252ba89e12c29a930ca75d21160e67b25cf72
aysenursezgin <115412288+aysenursezgin@users.noreply.github.com>
2024-07-16 20:54:47 +0300

    Proje dosyası ekle.

---
+++ IlkUygulamam/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aysenur Sezgin");
+            Console.WriteLine("Yazılım dünyasında yerimi almak için buradayım :)");
+
+        }
+    }
+}
